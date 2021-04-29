39af79ab0c7b565f9cd1e706a652e102eba1c939
Enrique Flores <kikeoffline@gmail.com>
2021-04-28 22:20:58 -0600

    📦️ Update dependencies

--- next.config.js
+++ next.config.js
@@ -1,10 +1,14 @@
-require('dotenv').config()
+require("dotenv").config();
 
 module.exports = {
+  future: {
+    webpack5: true,
+  },
   env: {
-    STRAPI_URL: process.env.STRAPI_URL
+    STRAPI_URL: process.env.STRAPI_URL,
+    NEXTJS_URL: process.env.NEXTJS_URL,
   },
   images: {
-    domains: ['localhost']
-  }
-}
+    domains: ["localhost"],
+  },
+};
