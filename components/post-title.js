3fe47aa4df2faf9fc019f532f0ab723a3dcfa2ab
Enrique Flores <kikeoffline@gmail.com>
2021-04-28 22:24:22 -0600

    🚚 Moved and renamed files and paths

--- components/post-title.js
+++
@@ -1,7 +0,0 @@
-export default function PostTitle ({ children }) {
-  return (
-    <h1 className='text-6xl md:text-7xl font-bold tracking-tighter leading-tight md:leading-none mb-12'>
-      {children}
-    </h1>
-  )
-}
